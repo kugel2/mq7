e397aed14ecccd435e1b9c86fe9ca5fc7f52c6ba
florian <florian@docwinkler.at>
2017-06-02 22:08:46 +0200

    add temperatur and humidity correction factors (extrapolated from datasheet)

--- MQ7.cpp
+++ MQ7.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+#include <math.h>
+
 #include "MQ7.h"
 
 MQ7::MQ7(const int heater_pin, const int adc_pin) {
@@ -25,4 +27,24 @@ void MQ7::heater_off() {
 int MQ7::read_adc() {
   int adc = analogRead(_adc);
   return adc;
+}
+
+float MQ7::getCorrectionFactor(float t, float h) {
+  // Constants interpolatet from MQ7 datasheet,
+  // humidity factors only good > 10°C
+  // TODO: better fitting curve for lower humidity
+  const float _a = 0.83169946;
+  const float _b = 0.04596768;
+  const float _c = -0.95765344;
+
+  const float _d = 0.14;
+
+  return _a+pow(M_E,_c+_b*(-t))-(h-0.33)*_d/0.52;
+
+  // ax.plot(df.x, _a+np.exp(_c+_b*-df.x)-(rh-.33)*0.14/.52
+}
+
+float MQ7::getResistance() {
+  const float _rload = 1.0;
+  return ((2000.0/(float)read_adc()) - 1.)*_rload;
 }
--- MQ7.h
+++ MQ7.h
@@ -1,11 +1,9 @@
 #pragma once
 
-
-
 class MQ7 {
 public:
   const int heater_high_pwm = 255;
-  const int heater_low_pwm = 72;
+  const int heater_low_pwm = 20;
   const int time_high = 60;
   const int time_low = 90;
 
@@ -15,6 +13,8 @@ public:
   void heater_low(void);
   void heater_off(void);
   int read_adc(void);
+  float getCorrectionFactor(float t, float h);
+  float getResistance(void);
 private:
   int _heater;
   int _adc;
